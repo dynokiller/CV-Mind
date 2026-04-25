6397530f8d2738f1c65cb60901eb5a12c31db564
unknown <devanshkoyani1@gmail.com>
2026-04-25 21:18:30 +0530

    Untrack old CV_MIND-main and update .gitignore

--- CV_MIND-main/CV_MIND-main/db.py
+++
@@ -1,2 +0,0 @@
-# Database has been completely removed to make the app Lightweight and Stateless.
-# This file is intentionally left blank.
--- CV_MIND-main/CV_MIND-main/mail_service/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from .otp_mail import build_otp_email
-from .reset_mail import build_reset_email
-
-HANDLERS = {
-    "OTP_VERIFY": build_otp_email,
-    "RESET_PASSWORD": build_reset_email
-}
